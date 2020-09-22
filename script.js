dc856cfd99535401b7b92061a8ee70d8c66943fd
mkarkuody <mkarkuody@yahoo.com>
2020-09-22 16:10:40 -0700

    did some final changes in javascript file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // created Variables and gave them strings, numbers ands Nulls
 var generateBtn = document.querySelector("#generate")
-var symbol = '"~?!@#$%^&*()_+:}|<>,"'
+var symbol = ';!@#$%^&*=+-_)(":;'
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
 var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 var numbers = '0123456789'
@@ -10,7 +10,7 @@ var useSelection = "";
 // created passwordGenerator Function to calculate and create my password?
 function passwordGenerator() {
     var passwordLength = askpasswordlength()
-    askCharectorType()
+    askForSelect();
     for (let i = password.length; i < passwordLength; i++) {
 
         password += getrandomCharector(useSelection);
@@ -20,14 +20,16 @@ function passwordGenerator() {
 }
 
 // Created welcome alert
-var welcome = alert('              <==Welcome to the Password Generator ==>                        <==you can create a uniqe passwork fast and easy in this page==> ');
 
 var properlength = false;
 // created askpasswordlength Function and made a prompt to make sure "users" choose between 8 and 128
+
+var welcome = alert('              <==Welcome to the Password Generator ==>                        <==you can create a uniqe passwork fast and easy in this page==> ');
+
 function askpasswordlength() {
 
     var pl = prompt('How many characters do you need in your password? (please choose between 8 to 128 characters)');
-    // return pl;
+    return pl;
 }
 var passwordLength = askpasswordlength()
 // created a while loop to keep asking user to follow the direction and chooce between 8 and 128
@@ -38,25 +40,17 @@ while (passwordLength < 8 || passwordLength > 128) {
 
 };
 
-// if (passwordLength > 7 || passwordLength < 129) {
-
-
-// var numbersCh = confirm('Do you like to have number(s) in your password?');
-// var lowerCaseCh = confirm('Do you like to have lowercase letter(s) in your password?');
-// var upperCaseCh = confirm('Do you like to have uppercase letter(s) in your password?');
-// var symbolCh = confirm('Do you like to have symbol(s) in your password?');
-// }
-
 // created a function to keep users on right path and have mutiple choices to creat there unique password 
-askForSelect();
+askForSelect ()
 function askForSelect() {
-    if (passwordLength > 7 || passwordLength < 129)
+  
+    if (passwordLength > 7 || passwordLength < 129) {
 
         var numbersCh = confirm('Do you like to have number(s) in your password?');
-    var lowerCaseCh = confirm('Do you like to have lowercase letter(s) in your password?');
-    var upperCaseCh = confirm('Do you like to have uppercase letter(s) in your password?');
-    var symbolCh = confirm('Do you like to have symbol(s) in your password?');
-
+        var lowerCaseCh = confirm('Do you like to have lowercase letter(s) in your password?');
+        var upperCaseCh = confirm('Do you like to have uppercase letter(s) in your password?');
+        var symbolCh = confirm('Do you like to have symbol(s) in your password?');
+    }
 
     if (!numbersCh && !lowerCaseCh && !upperCaseCh && !symbolCh) {
 
@@ -79,8 +73,8 @@ function askForSelect() {
 
     }
     if (symbolCh) {
-        password += getrandomCharector(Symbol)
-        useSelection += Symbol
+        password += getrandomCharector(symbol)
+        useSelection += symbol
 
     }
 
@@ -91,6 +85,7 @@ function getrandomCharector(charectorStrings) {
     var randomNubmer = Math.floor(Math.random() * charectorStrings.length);
     return charectorStrings[randomNubmer]
 }
+
 function writePassword() {
     var finalPassword = passwordGenerator();
     var passwordText = document.querySelector("#password")
